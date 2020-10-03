b3cda6517517965b5e83a5494ae8313f6f220174
mark <dev.kram5@gmail.com>
2020-10-03 10:37:20 +0800

    Added files

---
+++ XamarinUtility.Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XamarinUtility.Test
+{
+    class Program
+    {
+        static BindableProperty SubmitCommandProperty = BindableHelper.CreateProperty<string>();
+
+        static void Main()
+        {
+            var obj = new
+            {
+                a = "apple",
+                b = "ball",
+                c = "cat"
+            };
+            var result = QueryStringHelper.ToQueryString(obj);
+            var d = result;
+        }
+    }
+}
